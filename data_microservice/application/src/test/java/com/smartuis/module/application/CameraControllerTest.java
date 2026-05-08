29b0c540edcc53e16f686b386c490c556b3849c7
Pwned-Sec <75802180+PWN3D777@users.noreply.github.com>
2026-05-08 01:25:37 -0500

    Replace SpringBootTest with MockitoExtension in tests

--- data_microservice/application/src/test/java/com/smartuis/module/application/CameraControllerTest.java
+++ data_microservice/application/src/test/java/com/smartuis/module/application/CameraControllerTest.java
@@ -22,7 +22,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class CameraControllerTest {
 
     @Mock
